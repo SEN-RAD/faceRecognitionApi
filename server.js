6e4706bbb838afbadc262c9d462639ecbbe4b7b0
Andres Bermudez <afbg.art@gmail.com>
2022-12-26 10:30:52 +0100

    changing var

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcry
 app.get('/profile/:id', (req, res)=> { profile.handleProfile(req, res, db) })
 app.put('/image', (req, res)=> { image.handleImage(req, res, db) }) 
 
-app.listen(process.env.PORT || 3000, ()=>{
+app.listen(process.env.PORT, ()=>{
 	console.log(`server is listening on ${process.env.PORT}`);
 })
 
