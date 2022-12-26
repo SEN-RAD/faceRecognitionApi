3d368ef4f4b9da2ced6254edbf8a98bdea1e52bd
Andres Bermudez <afbg.art@gmail.com>
2022-12-26 10:34:01 +0100

    erasing changes

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcry
 app.get('/profile/:id', (req, res)=> { profile.handleProfile(req, res, db) })
 app.put('/image', (req, res)=> { image.handleImage(req, res, db) }) 
 
-app.listen(process.env.PORT, ()=>{
-	console.log(`server is listening on ${process.env.PORT}`);
+app.listen(3000, ()=>{
+	console.log('app is running on port 3000');
 })
 
